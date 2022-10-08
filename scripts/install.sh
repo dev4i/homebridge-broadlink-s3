51358e3b2196b0db1e1168bb44416a523d9e86c7
dev4i <chrisink22@yahoo.com>
2022-10-08 23:56:35 +0100

    t

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash 
-echo 'HELLO!!!'
+mkdir /var/lib/homebridge/python
+echo "import sys;
+import broadlink;
+import json;
+
+devices = broadlink.discover();
+listOfDeviceIds = [];
+for device in devices:
+    device.auth()
+    for subdevice in device.get_subdevices():
+        subdevice['host'] = device.host
+        subdevice['status'] = device.get_state(did=subdevice['did'])
+        listOfDeviceIds.append(subdevice)
+        # print(subdevice);
+print(json.dumps(listOfDeviceIds));
+sys.stdout.flush();" > /var/lib/homebridge/python/get-all-subdevices.py
