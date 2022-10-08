a9cfb89c10cbab3db1e99342eaed03bc3741d8f8
dev4i <chrisink22@yahoo.com>
2022-10-08 18:28:57 +0100

    cleaned up

--- src/LC1Switch1.ts
+++ src/LC1Switch1.ts
@@ -7,18 +7,14 @@ import child_process = require('child_process');
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class LC1Switch3 {
+export class LC1Switch1 {
   private serviceSwitchOne: Service;
-  private serviceSwitchTwo: Service;
-  private serviceSwitchThree: Service;
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
   private states = {
     pwr1: this.accessory.context.device.status.pwr1 !== void 0 ? this.accessory.context.device.status.pwr1 : null,
-    pwr2: this.accessory.context.device.status.pwr2 !== void 0 ? this.accessory.context.device.status.pwr2 : null,
-    pwr3: this.accessory.context.device.status.pwr3 !== void 0 ? this.accessory.context.device.status.pwr3 : null,
   };
 
   constructor(private readonly platform: ExampleHomebridgePlatform, private readonly accessory: PlatformAccessory) {
@@ -47,27 +43,6 @@ export class LC1Switch3 {
       .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
       .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
 
-    this.serviceSwitchTwo =
-      this.accessory.getService('Switch Two') ||
-      this.accessory.addService(this.platform.Service.Switch, 'Switch Two', this.accessory.context.device.did + '-2');
-
-    this.serviceSwitchTwo.setCharacteristic(this.platform.Characteristic.Name, 'Switch Two');
-
-    this.serviceSwitchTwo
-      .getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setTwoOn.bind(this))
-      .onGet(this.getTwoOn.bind(this));
-
-    this.serviceSwitchThree =
-      this.accessory.getService('Switch Three') ||
-      this.accessory.addService(this.platform.Service.Switch, 'Switch Three', this.accessory.context.device.did + '-3');
-
-    this.serviceSwitchThree.setCharacteristic(this.platform.Characteristic.Name, 'Switch Three');
-
-    this.serviceSwitchThree
-      .getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setThreeOn.bind(this))
-      .onGet(this.getThreeOn.bind(this));
     /**
      * Creating multiple services of the same type.
      *
@@ -114,66 +89,6 @@ export class LC1Switch3 {
     this.platform.log.debug('Set Characteristic On ->', value);
   }
 
-  async setTwoOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    // this.states.On = value as boolean;
-
-    const intValue = (value as boolean) === true ? 1 : 0;
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [
-        'src/set-device-state.py',
-        this.accessory.context.device.host,
-        this.accessory.context.device.uniqueId,
-        intValue,
-        'pwr2',
-      ],
-      { shell: false },
-    );
-
-    pythonProcess.stdout.on('data', (data) => {
-      this.platform.log.info('Python output:');
-      this.platform.log.info(data.toString());
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-      this.platform.log.error('Python error:');
-      this.platform.log.error(data.toString());
-    });
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
-
-  async setThreeOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    // this.states.On = value as boolean;
-
-    const intValue = (value as boolean) === true ? 1 : 0;
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [
-        'src/set-device-state.py',
-        this.accessory.context.device.host,
-        this.accessory.context.device.uniqueId,
-        intValue,
-        'pwr3',
-      ],
-      { shell: false },
-    );
-
-    pythonProcess.stdout.on('data', (data) => {
-      this.platform.log.info('Python output:');
-      this.platform.log.info(data.toString());
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-      this.platform.log.error('Python error:');
-      this.platform.log.error(data.toString());
-    });
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
-
   /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
@@ -194,70 +109,6 @@ export class LC1Switch3 {
     const pythonProcess = child_process.spawn(
       'python3',
       [ 'src/get-device-state.py', this.accessory.context.device.host, this.accessory.context.device.uniqueId, 'pwr1' ],
-      { shell: false },
-    );
-
-    return await new Promise((resolve) => {
-      pythonProcess.stdout.on('data', (data) => {
-        this.platform.log.info('Python output:');
-        const response = data.toString();
-        if (response.trim() === '1') {
-          this.platform.log.debug('Get Characteristic On ->', true);
-          resolve(true);
-        }
-        if (response.trim() === '0') {
-          this.platform.log.debug('Get Characteristic On ->', false);
-          resolve(false);
-        }
-      });
-
-      pythonProcess.stderr.on('data', (data) => {
-        this.platform.log.error('Python error:');
-        this.platform.log.error(data.toString());
-        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-      });
-    });
-  }
-
-  async getTwoOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    // const isOn = this.states.On;
-
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [ 'src/get-device-state.py', this.accessory.context.device.host, this.accessory.context.device.uniqueId, 'pwr2' ],
-      { shell: false },
-    );
-
-    return await new Promise((resolve) => {
-      pythonProcess.stdout.on('data', (data) => {
-        this.platform.log.info('Python output:');
-        const response = data.toString();
-        if (response.trim() === '1') {
-          this.platform.log.debug('Get Characteristic On ->', true);
-          resolve(true);
-        }
-        if (response.trim() === '0') {
-          this.platform.log.debug('Get Characteristic On ->', false);
-          resolve(false);
-        }
-      });
-
-      pythonProcess.stderr.on('data', (data) => {
-        this.platform.log.error('Python error:');
-        this.platform.log.error(data.toString());
-        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-      });
-    });
-  }
-
-  async getThreeOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    // const isOn = this.states.On;
-
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [ 'src/get-device-state.py', this.accessory.context.device.host, this.accessory.context.device.uniqueId, 'pwr3' ],
       { shell: false },
     );
 
--- src/LC1Switch2.ts
+++ src/LC1Switch2.ts
@@ -7,10 +7,9 @@ import child_process = require('child_process');
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class LC1Switch3 {
+export class LC1Switch2 {
   private serviceSwitchOne: Service;
   private serviceSwitchTwo: Service;
-  private serviceSwitchThree: Service;
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
@@ -18,7 +17,6 @@ export class LC1Switch3 {
   private states = {
     pwr1: this.accessory.context.device.status.pwr1 !== void 0 ? this.accessory.context.device.status.pwr1 : null,
     pwr2: this.accessory.context.device.status.pwr2 !== void 0 ? this.accessory.context.device.status.pwr2 : null,
-    pwr3: this.accessory.context.device.status.pwr3 !== void 0 ? this.accessory.context.device.status.pwr3 : null,
   };
 
   constructor(private readonly platform: ExampleHomebridgePlatform, private readonly accessory: PlatformAccessory) {
@@ -58,16 +56,6 @@ export class LC1Switch3 {
       .onSet(this.setTwoOn.bind(this))
       .onGet(this.getTwoOn.bind(this));
 
-    this.serviceSwitchThree =
-      this.accessory.getService('Switch Three') ||
-      this.accessory.addService(this.platform.Service.Switch, 'Switch Three', this.accessory.context.device.did + '-3');
-
-    this.serviceSwitchThree.setCharacteristic(this.platform.Characteristic.Name, 'Switch Three');
-
-    this.serviceSwitchThree
-      .getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setThreeOn.bind(this))
-      .onGet(this.getThreeOn.bind(this));
     /**
      * Creating multiple services of the same type.
      *
@@ -144,36 +132,6 @@ export class LC1Switch3 {
     this.platform.log.debug('Set Characteristic On ->', value);
   }
 
-  async setThreeOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    // this.states.On = value as boolean;
-
-    const intValue = (value as boolean) === true ? 1 : 0;
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [
-        'src/set-device-state.py',
-        this.accessory.context.device.host,
-        this.accessory.context.device.uniqueId,
-        intValue,
-        'pwr3',
-      ],
-      { shell: false },
-    );
-
-    pythonProcess.stdout.on('data', (data) => {
-      this.platform.log.info('Python output:');
-      this.platform.log.info(data.toString());
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-      this.platform.log.error('Python error:');
-      this.platform.log.error(data.toString());
-    });
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
-
   /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
@@ -226,38 +184,6 @@ export class LC1Switch3 {
     const pythonProcess = child_process.spawn(
       'python3',
       [ 'src/get-device-state.py', this.accessory.context.device.host, this.accessory.context.device.uniqueId, 'pwr2' ],
-      { shell: false },
-    );
-
-    return await new Promise((resolve) => {
-      pythonProcess.stdout.on('data', (data) => {
-        this.platform.log.info('Python output:');
-        const response = data.toString();
-        if (response.trim() === '1') {
-          this.platform.log.debug('Get Characteristic On ->', true);
-          resolve(true);
-        }
-        if (response.trim() === '0') {
-          this.platform.log.debug('Get Characteristic On ->', false);
-          resolve(false);
-        }
-      });
-
-      pythonProcess.stderr.on('data', (data) => {
-        this.platform.log.error('Python error:');
-        this.platform.log.error(data.toString());
-        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-      });
-    });
-  }
-
-  async getThreeOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    // const isOn = this.states.On;
-
-    const pythonProcess = child_process.spawn(
-      'python3',
-      [ 'src/get-device-state.py', this.accessory.context.device.host, this.accessory.context.device.uniqueId, 'pwr3' ],
       { shell: false },
     );
 
