71d20dba9e2671b5a785cb04f242e69cb52eeea2
dev4i <chrisink22@yahoo.com>
2022-10-14 09:04:46 +0100

    fixed bug in python code. Only S3 Hubs and their sub devices should now be detected

--- broadlink-s3-python/get-all-subdevices.py
+++ broadlink-s3-python/get-all-subdevices.py
@@ -5,11 +5,12 @@ import json;
 devices = broadlink.discover();
 listOfDeviceIds = [];
 for device in devices:
-    device.auth()
-    for subdevice in device.get_subdevices():
-        subdevice['host'] = device.host
-        subdevice['status'] = device.get_state(did=subdevice['did'])
-        listOfDeviceIds.append(subdevice)
-        # print(subdevice);
+    if type(devices[0]).__name__ == 's3':
+        device.auth()
+        for subdevice in device.get_subdevices():
+            subdevice['host'] = device.host
+            subdevice['status'] = device.get_state(did=subdevice['did'])
+            listOfDeviceIds.append(subdevice)
+            # print(subdevice);
 print(json.dumps(listOfDeviceIds));
 sys.stdout.flush();
