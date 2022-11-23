81ee3f13eee670dc40e0dbdb1681e608c1fe5f4b
dev4i <chrisink22@yahoo.com>
2022-11-23 10:16:09 +0000

    added upgrade flag for broadlink python module

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@ mkdir /var/lib/homebridge/broadlink-s3-python
 cp broadlink-s3-python/get-all-subdevices.py /var/lib/homebridge/broadlink-s3-python/get-all-subdevices.py
 cp broadlink-s3-python/get-device-state.py /var/lib/homebridge/broadlink-s3-python/get-device-state.py
 cp broadlink-s3-python/set-device-state.py /var/lib/homebridge/broadlink-s3-python/set-device-state.py
-pip3 install broadlink
+pip3 install --upgrade broadlink
